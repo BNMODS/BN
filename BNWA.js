f4c4fa86bb40cb9906a4a8d3ff45d603acab0a95
BNMODS <87958005+BNMODS@users.noreply.github.com>
2021-09-20 00:25:57 -0400

    Update BNWA.js

--- BNWA.js
+++ BNWA.js
@@ -1,18 +1,18 @@
 {
-	"version": "18.0",
-	"download": "https://ymmods-official.blogspot.com/2021/04/ymwa.html",
+	"version": "10.0",
+	"download": "https://t.me/berllinmods",
 	"update": [
 		{
 			"languaje": "en",
-			"changelog": "https://raw.githubusercontent.com/JesusMuentes/YM/main/changelog-en.html"
+			"changelog": "https://raw.githubusercontent.com/BNMODS/BN/main/bnwaupdate-w.html"
 		},
 		{
 			"languaje": "es",
-			"changelog": "https://raw.githubusercontent.com/JesusMuentes/YM/main/changelog-es.html"
+			"changelog": "https://raw.githubusercontent.com/BNMODS/BN/main/bnwaupdate-w.html"
 		},
 		{
 			"languaje": "pt",
-			"changelog": "https://raw.githubusercontent.com/JesusMuentes/YM/main/changelog-pt.html"
+			"changelog": "https://raw.githubusercontent.com/BNMODS/BN/main/bnwaupdate-w.html"
 		}
 	]
 }
